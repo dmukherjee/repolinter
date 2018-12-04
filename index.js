3c2e5ee7d7353194686d6b9e86a961a074077af1
debalina <mukherjee.debalina@gmail.com>
2018-12-03 16:57:54 -0800

    (feat) update index.js to run custom rules by default

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ module.exports.lint = function lint (targetDir, filterPaths = [], ruleset = null
   if (!ruleset) {
     let rulesetPath = findConfig('repolint.json', { cwd: targetDir })
     rulesetPath = rulesetPath || findConfig('repolinter.json', { cwd: targetDir })
-    rulesetPath = rulesetPath || path.join(__dirname, 'rulesets/default.json')
+    // path to default ruleset
+    // rulesetPath = rulesetPath || path.join(__dirname, 'rulesets/default.json')
+    //  path to custom ruleset
+    rulesetPath = rulesetPath || path.join(__dirname, 'rulesets/custom.json')
     exports.outputInfo(`Ruleset: ${path.relative(targetDir, rulesetPath)}`)
     ruleset = jsonfile.readFileSync(rulesetPath)
   }
